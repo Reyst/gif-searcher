6ef1f37be958ec5f22e682fae935bbf57d5af7bd
Pavlo Oleksenko <oleksenko.pm@gmail.com>
2024-09-24 14:53:59 +0300

    update appearance of the search screen

--- app/src/main/java/com/github/reyst/giphyapp/ui/search/SearchActivity.kt
+++ app/src/main/java/com/github/reyst/giphyapp/ui/search/SearchActivity.kt
@@ -2,34 +2,60 @@ package com.github.reyst.giphyapp.ui.search
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
+import com.github.reyst.giphyapp.R
 import com.github.reyst.giphyapp.domain.entities.GiphyGif
+import com.github.reyst.giphyapp.domain.entities.ImageVariants
+import com.github.reyst.giphyapp.domain.exceptions.GiphyApiKeyException
+import com.github.reyst.giphyapp.domain.exceptions.GiphyBadSearchRequest
+import com.github.reyst.giphyapp.domain.exceptions.GiphyDownstreamSystemException
+import com.github.reyst.giphyapp.domain.exceptions.TooLongQueryException
 import com.github.reyst.giphyapp.ui.details.DetailsActivity
 import com.github.reyst.giphyapp.ui.theme.SearcherTheme
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchActivity : ComponentActivity() {
@@ -74,117 +100,136 @@ fun SearchScreen(
 
     }
 
-    Column(modifier = modifier.fillMaxSize()) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
 
-        OutlinedTextField(queryState.value, onValueChange = onQueryChange)
+        OutlinedTextField(
+            queryState.value,
+            onValueChange = onQueryChange,
+            label = { Text(stringResource(R.string.query_caption)) },
+            trailingIcon = {
+                IconButton(onClick = { onQueryChange("") }) {
+                    Icon(imageVector = Icons.Default.Clear, null)
+                }
 
-        LazyVerticalGrid(
-            columns = columnsCount,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
 
-            items(pagingItems.itemCount) { index ->
+            },
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
 
-                pagingItems[index]
-                    ?.let {
-                        AsyncImage(
-                            model = it.images.original.url,
-                            contentDescription = it.title,
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            LazyVerticalGrid(
+                columns = columnsCount,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
+
+                modifier = Modifier.fillMaxSize(),
+            ) {
+
+                items(
+                    pagingItems.itemCount,
+                    key = { pagingItems[it]?.id.orEmpty() },
+                ) { index ->
+
+                    pagingItems[index]
+                        ?.let {
+                            AsyncImage(
+                                model = it.images.original.url,
+                                contentDescription = it.title,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(100.dp)
+                                    .clickable { onItemClick(it) }
+                            )
+                        }
+                        ?: Spacer(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .height(100.dp)
-                                .clickable { onItemClick(it) }
                         )
-                    }
-                    ?: Spacer(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(100.dp)
-                    )
+                }
+            }
+
+            val loadState = pagingItems.loadState
+            if (loadState.refresh == LoadState.Loading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .padding(top = 48.dp)
+                        .size(128.dp)
+                        .align(alignment = Alignment.TopCenter)
+                )
+            }
+
+            if (loadState.append == LoadState.Loading || loadState.prepend == LoadState.Loading) {
+                LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+            }
+
+            val ctx = LocalContext.current
+
+            if (loadState.hasError) {
+                LaunchedEffect(loadState) {
+                    listOf(loadState.refresh, loadState.append, loadState.prepend)
+                        .filterIsInstance<LoadState.Error>()
+                        .map {
+                            when (it.error) {
+                                is GiphyApiKeyException -> ctx.getString(R.string.key_exception)
+
+                                is GiphyBadSearchRequest,
+                                is TooLongQueryException -> ctx.getString(R.string.check_query)
+
+                                is GiphyDownstreamSystemException -> ctx.getString(R.string.service_down)
+                                else -> ctx.getString(R.string.general_error)
+                            }
+                        }
+                        .takeIf { it.isNotEmpty() }
+                        ?.joinToString(separator = "\n")
+                        ?.also { Toast.makeText(ctx, it, Toast.LENGTH_LONG).show() }
+                }
             }
         }
     }
 }
 
+@Preview
+@Composable
+fun SearchScreenPreview() {
 
-/*
-    private val binding by lazy { ActivitySearchBinding.inflate(layoutInflater) }
+    SearcherTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+            val qState = remember { mutableStateOf("Car") }
 
-    private val adapter by lazy {
-        SearchResultAdapter { DetailsActivity.start(this, it) }
-    }
+            val flow = remember {
+                val fakeData = listOf(
+                    GiphyGif(
+                        id = "id1",
+                        url = "url",
+                        images = ImageVariants()
+                    ),
+                    GiphyGif(
+                        id = "id2",
+                        url = "url",
+                        images = ImageVariants()
+                    ),
+                )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        initViews()
-        initSubscriptions()
-    }
-
-    private fun initViews() {
-        val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-        binding.searchResultGrid.layoutManager = GridLayoutManager(this, if (isLandscape) 5 else 3)
-        binding.searchResultGrid.adapter = adapter
-    }
-
-    private fun initSubscriptions() {
-
-        binding.queryText
-            .obtainChangesFlow()
-            .onEach { vm.updateQuery(it.toString()) }
-            .launchIn(lifecycleScope)
-
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED) {
-                vm.gifItems.collect(adapter::submitData)
+                val pagingData = PagingData.from(fakeData)
+                MutableStateFlow(pagingData)
             }
-        }
 
-        lifecycleScope.launch {
-            adapter.loadStateFlow.collectLatest { loadStates ->
-
-                binding.refreshProgress.isVisible = loadStates.refresh == LoadState.Loading
-
-                binding.updateProgress.isVisible =
-                    loadStates.append == LoadState.Loading || loadStates.prepend == LoadState.Loading
-
-                if (loadStates.hasError) {
-
-                    listOf(loadStates.refresh, loadStates.append, loadStates.prepend)
-                        .filterIsInstance<LoadState.Error>()
-//                        .filter { !it.endOfPaginationReached }
-                        .map {
-
-                            Log.wtf("INSPECT", "error: ${it.error}", it.error)
-
-                            when (it.error) {
-                                is GiphyApiKeyException -> getString(R.string.key_exception)
-
-                                is GiphyBadSearchRequest,
-                                is TooLongQueryException -> getString(R.string.check_query)
-
-                                is GiphyDownstreamSystemException -> getString(R.string.service_down)
-                                else -> getString(R.string.general_error)
-                            }
-                        }
-                        .takeIf { it.isNotEmpty() }
-                        ?.joinToString(separator = "\n")
-                        ?.also {
-                            Toast.makeText(this@SearchActivity, it, Toast.LENGTH_LONG).show()
-                        }
-                }
-            }
+            SearchScreen(
+                queryState = qState,
+                onQueryChange = { qState.value = it },
+                pagingItems = flow.collectAsLazyPagingItems(),
+                modifier = Modifier.padding(innerPadding),
+                onItemClick = { },
+            )
         }
     }
-*/
 
+}
