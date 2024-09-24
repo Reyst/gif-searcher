a5970e57580079ffc146b031f2566e34c2c590f9
Pavlo Oleksenko <oleksenko.pm@gmail.com>
2024-09-24 15:57:14 +0300

    update appearance of the details screen

--- app/src/main/java/com/github/reyst/giphyapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/github/reyst/giphyapp/ui/details/DetailsActivity.kt
@@ -8,28 +8,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -133,7 +149,11 @@ private fun GiphyGifImage(url: String, modifier: Modifier = Modifier) {
 
 @Composable
 private fun PropertiesList(options: List<DetailOption>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp),
+        modifier = modifier
+    ) {
         items(options) { item ->
             when (item) {
                 is General -> SimpleProperty(item)
@@ -151,27 +171,60 @@ fun ImageProperty(option: Image) {
             .wrapContentHeight()
     ) {
         val imageData = option.value
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-        ) {
-            Text(text = option.title)
-            Spacer(modifier = Modifier.padding(vertical = 8.dp))
 
-            Text(text = "${imageData.width} x ${imageData.height}")
+        CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.bodyLarge) {
 
-            Text(text = getSizeString("size", imageData.size))
-            Text(text = getUrlString("url", imageData.url))
-            Text(text = getSizeString("MP4 size", imageData.mp4Size))
-            Text(text = getUrlString("MP4 url", imageData.mp4Url))
-            Text(text = getSizeString("Webp size", imageData.webpSize))
-            Text(text = getUrlString("Webp url", imageData.webpUrl))
+            Column(
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .wrapContentHeight()
+            ) {
+                Text(
+                    text = option.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.SemiBold,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(text = "${imageData.width} x ${imageData.height}")
+
+                Text(text = getSizeString("size", imageData.size))
+                UrlText("url", imageData.url)
+                Text(text = getSizeString("MP4 size", imageData.mp4Size))
+                UrlText("MP4 url", imageData.mp4Url)
+                Text(text = getSizeString("Webp size", imageData.webpSize))
+                UrlText("Webp url", imageData.webpUrl)
+            }
         }
     }
 }
 
-private fun getUrlString(title: String, url: String) = "$title: ${url.ifBlank { " - " }}"
+@Composable
+fun UrlText(title: String, url: String) {
+    val uriHandler = LocalUriHandler.current
+    Text(
+        text = buildAnnotatedString {
+            append("$title: ")
+
+            if (url.isBlank()) append(" - ")
+            else withLink(
+                LinkAnnotation.Url(
+                    url = url,
+                    styles = TextLinkStyles(
+                        SpanStyle(
+                            color = Color.Blue,
+                            textDecoration = TextDecoration.Underline
+                        )
+                    ),
+                    linkInteractionListener = { uriHandler.openUri(url) }
+                )
+            ) { append(url) }
+        }
+    )
+}
+
 private fun getSizeString(title: String, size: Int): String {
     val sizeStr = size.takeIf { it > 0 }
         ?.toString()
@@ -189,18 +242,27 @@ fun SimpleProperty(options: General) {
             .wrapContentHeight()
     ) {
         Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(16.dp)
                 .wrapContentHeight()
         ) {
-            Text(text = "${options.title}:", modifier = Modifier.wrapContentSize())
             Text(
-                text = options.value, modifier = Modifier
+                text = "${options.title}:",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.wrapContentSize()
+            )
+            Text(
+                text = options.value,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
             )
         }
-
     }
 }
 
@@ -213,7 +275,7 @@ fun DetailsScreenPreview() {
                 state = remember {
                     mutableStateOf(
                         DetailsState(
-                            url = "https://media0.giphy.com/media/1X2K7D9txBkDS/giphy.gif?cid=e46b4c94hfo5whmf6juwq1sp2ygdtcr6ybdlwqycan62vf2b&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                            url = "https://media0.giphy.com/media/1X2K7D9txBkDS/giphy.gif",
                             options = listOf(
                                 General("Title", "title"),
                                 Image(
